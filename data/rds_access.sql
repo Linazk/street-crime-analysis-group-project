658e8b2ccb3684e46ec99f773eac6723f482da56
Lina Ziaka <74008585+Linazk@users.noreply.github.com>
2022-01-02 16:47:32 +0200

    Delete rds_access.sql

--- data/rds_access.sql
+++
@@ -1,5 +0,0 @@
--- for RDS; giving access
-CREATE USER 'cfg_user'@'%' IDENTIFIED BY 'cfg_project2021';
-SHOW GRANTS for cfg_user;
-GRANT ALL PRIVILEGES ON *.*  TO 'admin'@'localhost';
-FLUSH PRIVILEGES;
